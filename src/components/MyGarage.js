f7b6201ff800191a051a13c0e01b0acdb7df523f
Pauline Tannos <ptannos2@gmail.com>
2021-08-28 22:38:15 -0400

    {updated}: added options to MyGarage component

--- src/components/MyGarage.js
+++ src/components/MyGarage.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const MyGarage = () => {
-  return <div>Here is my garage</div>;
+  return (
+    <>
+      <h2>My Garage</h2>
+      <p>View garage</p>
+      <p>Find a box</p>
+      <p>Find an item</p>
+      <hr />
+      <p>Store a box</p>
+      <p>Store an item</p>
+      <hr />
+    </>
+  );
 };
 
 export default MyGarage;
